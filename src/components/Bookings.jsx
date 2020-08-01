162e83496d03c4fda73a281f747b7b78091f7230
gursidak2000@gmail.com <gursidak2000@gmail.com>
2020-08-01 22:52:16 +0530

    pushing some changes in booking css

--- src/components/Bookings.jsx
+++ src/components/Bookings.jsx
@@ -8,11 +8,22 @@ import { faCalendarAlt , faCheckCircle } from '@fortawesome/free-solid-svg-icons
 class Bookings extends Component {
     constructor(props) {
         super(props)
-        this.state = { activeTab: 0 ,
+        this.state = { activeTab: 1 ,
+                       activeContent:0,
             pastBookings :[
                 { key:1, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
                 { key:2, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
-                { key:3, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"}
+                { key:3, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:4, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},                
+                { key:5, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:6, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:7, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:8, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},                
+                { key:9, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:10, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},                
+                { key:11, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:12, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"},
+                { key:13, StationName:"Gursidak Gas Station", date:"July 26,2020", paid:"₹400"}
 
 
             ],
@@ -42,7 +53,6 @@ class Bookings extends Component {
 
                     return(
                         < ListItem key={vendor.key} className='' threeLine >
-                        {/* <img src={vendor.img} height="60px" width="80px" className="listItemImg" /> */}
                         <ListItemContent subtitle={vendor.date}> <a>{vendor.StationName}</a>
                         </ListItemContent>
                         <ListItemAction>
@@ -89,8 +99,8 @@ class Bookings extends Component {
         return (
             <div className="demo-tabs">
                 <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>Past</Tab>
-                    <Tab>Active</Tab>
+                    <Tab  >Past</Tab>
+                    <Tab  >Active</Tab>
                 </Tabs>
                 <section className="bookings" >
                     {this.renderContent()}
@@ -101,3 +111,9 @@ class Bookings extends Component {
 }
 
 export default Bookings;
+
+
+
+
+// onClick={ ()=>{this.setState({ activeContent : 1  });} }
+// onClick={ ()=>{this.setState({ activeContent : 2  });} }
